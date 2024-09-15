a543d8f3566de3804c0aa1e30c251f1959e19660
Sergii Marchenko <marikmarchelo@gmail.com>
2024-09-15 20:11:27 +0300

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
+import React from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <h1>PhoneBook</h1>
       <ContactForm />
       <SearchBox />
       <ContactList />
-    </>
+    </Provider>
   );
 };
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,15 +4,17 @@ import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
-// Конфігурація для збереження контактів
+// Конфігурація для збереження стану контактів
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["contacts"], // Переконайтесь, що ви зберігаєте тільки потрібні частини стану
+  whitelist: ["contacts"], // Вказуємо, що ми хочемо зберігати тільки contacts
 };
 
+// Створюємо персистований ред'юсер для контактів
 const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
+// Створюємо Redux store з персистованими контактами та фільтрами
 const store = configureStore({
   reducer: {
     contacts: persistedContactsReducer,
@@ -20,9 +22,9 @@ const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false, // Ігнорування перевірок серіалізації для `redux-persist`
+      serializableCheck: false, // Вимикаємо перевірки серіалізації для redux-persist
     }),
 });
 
 export const persistor = persistStore(store);
-export default store;
+export default store; // Експортуємо store за замовчуванням
