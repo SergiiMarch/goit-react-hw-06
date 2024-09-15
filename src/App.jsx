15f968134d5931501c2d3433f7b747fc228a9a1b
Sergii Marchenko <marikmarchelo@gmail.com>
2024-09-15 20:22:15 +0300

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import { Provider } from "react-redux"; // Важливо використовувати саме Provider
+import store from "./redux/store"; // Правильний імпорт store
+import { PersistGate } from "redux-persist/integration/react"; // Для забезпечення збереження стану
+import { persistor } from "./redux/store"; // Імпортуйте persistor
 
 const App = () => {
   return (
     <Provider store={store}>
-      <h1>PhoneBook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+      <PersistGate loading={null} persistor={persistor}>
+        <h1>PhoneBook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </PersistGate>
     </Provider>
   );
 };
